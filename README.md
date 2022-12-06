960244abcbcf359bf9bf5135fd19dd0ddb5d8284
eifr <ofir@vaultplatform.com>
2022-12-06 20:39:51 +0700

    add readme, fixed app.tsx

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # styling-direction-RFC
+1. clone
+1. `npm install`
+2. `npm run dev`
--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
-import './App.css';
+import classes from './App.module.css';
 import { Button } from './components/Button/Button';
 import { Header } from './components/Header/Header';
 
 function App() {
   return (
-    <div className='App'>
-      <Header size={2}>test</Header>
-      <Button>title</Button>
+    <div className={classes.root}>
+      <Header size={2}>Title</Header>
+      <Button>Button</Button>
     </div>
   );
 }
